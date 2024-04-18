4e823586cb90098e73434f5cf0aedb1a17bfdc94
Marco-Lombardi <marcolombardi25@libero.it>
2024-04-18 18:37:47 +0200

    Add Exercise 2.3 and 2.4

--- Esercizi/Lezione2/lezione2.py
+++ Esercizi/Lezione2/lezione2.py
@@ -2,3 +2,26 @@
 # 18/04/2024
 
 print("Hello World!")
+
+# Exercise 2.3
+
+#This variable contains a name
+
+name: str = "Mario"
+
+message: str = f"Ciao {name} ti va di imparare un po di python insieme"
+
+print(message)
+
+#Exercise 2.4 
+
+name: str = "Mario"
+
+# This variable print a name in lowercase
+
+name_lower: str = name.lower()
+
+# This variable print a name in uppercase
+name_upper: str = name.upper()
+
+print(f"{name}, {name_upper}, {name_lower},")
