bae85934cb65c2544e2e26325d26d3b40b3e918c
Marco-Lombardi <marcolombardi25@libero.it>
2024-04-19 18:27:57 +0200

    Second Function

--- Esercizi/Lezione3/Esercizio2.py
+++ Esercizi/Lezione3/Esercizio2.py
@@ -8,4 +8,18 @@ print(operation)
 
 print(subtract(10,5))
 
+# Second exercise
+def median(l: list[float]) -> float:
+
+ l = sorted(l)
+ mid = len(l) // 2
+
+ if len (l) % 2 != 0: # dispari
+  mediana = l[mid]
+ else: #pari
+  mediana = l[mid] + l[mid - 1]
+
+ return mediana
+
+
 
