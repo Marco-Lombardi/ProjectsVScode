843d707955b3a92deb0be05246bbdceca4e4a92f
Marco-Lombardi <marcolombardi25@libero.it>
2024-04-23 11:20:22 +0200

    Exercise 4.2

--- Esercizi/Lezione3/Es-Lezione3.py
+++ Esercizi/Lezione3/Es-Lezione3.py
@@ -6,6 +6,17 @@ Pizza: list = ["Margherita", "Capricciosa", "Boscaiola"]
 for pizza in Pizza:
     print(pizza)
 
+#Exercise 4.2: Animals
+Animals: list = ["Gatto", "Leone", "Ghepardo"]
+for felino in Animals:
+    print(felino)
     
+
+for felino in Animals:
+    print(f'Il {felino} è un felino')
+#Exercise uncomplete
+
+        
+
     
 
