8b6af98d317eb192c2b679a65b5a1ef45b612b92
Marco-Lombardi <marcolombardi25@libero.it>
2024-04-30 17:23:00 +0200

    Exercise 8.1

---
+++ Esercizi/Lezione 4/Es-Lezione 4.py
@@ -0,0 +1,9 @@
+#Marco Lombardi
+
+#Exercise 8.1: Message
+def display_message():
+    #Prints a message about what is being learned in this chapter
+    print("In this chapter, we are learning about functions in Python.")
+
+# Calling the function
+display_message()
