de2b844e2da2205c3a24c8ee8f7efbdadd6b8faa
Marco-Lombardi <marcolombardi25@libero.it>
2024-04-30 17:33:50 +0200

    Exercise 8.3

--- Esercizi/Lezione 4/Es-Lezione 4.py
+++ Esercizi/Lezione 4/Es-Lezione 4.py
@@ -16,3 +16,15 @@ def favorite_book(title):
 
 # Calling the function with a book title
 favorite_book("Alice in Wonderland")
+
+#Exercise 8.3: T-Shirt
+
+def make_shirt(size, message):
+    #Prints a sentence summarizing the size of the shirt and the message printed on it.
+    print(f"A {size} shirt will be made with the message: '{message}'.")
+
+# Calling the function once using positional arguments
+make_shirt("medium", "Hello, World!")
+
+# Calling the function a second time using keyword arguments
+make_shirt(size="large", message="Python Rocks!")
