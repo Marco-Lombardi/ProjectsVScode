9db1139c2575726a35e47000fbeb3cc62df338c8
Marco-Lombardi <marcolombardi25@libero.it>
2024-04-30 16:59:38 +0200

    Exercise 5.10

--- Esercizi/Lezione3/Es-Lezione3.py
+++ Esercizi/Lezione3/Es-Lezione3.py
@@ -332,6 +332,34 @@ for username in usernames:
     else:
         print(f"Hello {username}, thank you for logging in again.")
 
+#Exercise 5.9: No Admin 
+#I Understand this Exercise
+
+#Exercise 5.10: Checking Username
+# Define the list of current users
+current_users = ['John', 'Jane', 'Emma', 'Michael', 'Olivia']
+
+# Define the list of new users
+new_users = ['Emily', 'Jason', 'Jane', 'Liam', 'Olivia']
+
+# Convert current_users to lowercase for case-insensitive comparison
+current_users_lower = [user.lower() for user in current_users]
+
+# Loop through the new_users list
+for new_user in new_users:
+    # Convert the new username to lowercase for case-insensitive comparison
+    new_user_lower = new_user.lower()
+    
+    # Check if the new username has already been used
+    if new_user_lower in current_users_lower:
+        print(f"Sorry, the username '{new_user}' is already taken. Please enter a new username.")
+    else:
+        print(f"The username '{new_user}' is available.")
+
+
+
+
+
 
 
 
