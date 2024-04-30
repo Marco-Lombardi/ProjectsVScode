40b3645224ddf7b7d7a8370b34e22b9ad8787ff5
Marco-Lombardi <marcolombardi25@libero.it>
2024-04-30 17:45:19 +0200

    Exercise 8.4

--- Esercizi/Lezione 4/Es-Lezione 4.py
+++ Esercizi/Lezione 4/Es-Lezione 4.py
@@ -28,3 +28,18 @@ make_shirt("medium", "Hello, World!")
 
 # Calling the function a second time using keyword arguments
 make_shirt(size="large", message="Python Rocks!")
+
+#Exercise 8.4: Large Shirts
+
+def make_shirt(size="large", message="I love Python"):
+    #Prints a sentence summarizing the size of the shirt and the message printed on it.
+    print(f"A {size} shirt will be made with the message: '{message}'.")
+
+# Making a large shirt with the default message
+make_shirt()
+
+# Making a medium shirt with the default message
+make_shirt(size="medium")
+
+# Making a shirt of any size with a different message
+make_shirt(size="small", message="Keep Calm and Code On!")
