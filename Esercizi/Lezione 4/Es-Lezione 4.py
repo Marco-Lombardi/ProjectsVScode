188e05f50996da254cf9868b738f6dbb594cb8a7
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-01 19:22:48 +0200

    Exercise 8.14

--- Esercizi/Lezione 4/Es-Lezione 4.py
+++ Esercizi/Lezione 4/Es-Lezione 4.py
@@ -189,4 +189,17 @@ def build_profile(first_name, last_name, **kwargs):
 my_profile = build_profile("John", "Doe", age=30, hair="black", weight=75)
 print(my_profile)
 
+#Exercise 8.14: Cars
+
+def make_car(manufacturer, model, **kwargs):
+    car_info = {'manufacturer': manufacturer, 'model': model}
+    car_info.update(kwargs)
+    return car_info
+
+# Call the function with required information and additional name-value pairs
+car = make_car('subaru', 'outback', color='blue', tow_package=True)
+
+# Print the dictionary to ensure all information was stored correctly
+print(car)
+
 
