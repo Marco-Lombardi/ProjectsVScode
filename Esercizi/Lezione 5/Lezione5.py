d7914b780d9f794097fe383f627aa5aeff8e7416
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-06 11:43:06 +0200

    Exercise 1

--- Esercizi/Lezione 5/Lezione5.py
+++ Esercizi/Lezione 5/Lezione5.py
@@ -1,6 +1,19 @@
 # Marco Lombardi
 # 02-05-2024
 
+# Exercise 1
+# Scrivi una funzione che, data una lista, ritorni un dictionary che mappa ogni elemento alla sua frequenza nella lista.
+
+def frequency_dict(elements: list) -> dict:
+      frequencies = {}
+      for element in elements:
+        if element in frequencies:
+            frequencies[element] += 1
+        else:
+            frequencies[element] = 1
+      return frequencies
+    
+print(frequency_dict(['mela', 'banana', 'mela']))
 
 	
 
