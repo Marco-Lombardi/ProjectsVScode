1961ddf81f55211224a43441f59804a7b30175b9
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-06 11:47:47 +0200

    Exercise 2

--- Esercizi/Lezione 5/Lezione5.py
+++ Esercizi/Lezione 5/Lezione5.py
@@ -15,6 +15,25 @@ def frequency_dict(elements: list) -> dict:
     
 print(frequency_dict(['mela', 'banana', 'mela']))
 
+# Exercise 2
+
+# Scrivi una funzione che ruota gli elementi di una lista verso sinistra di un numero specificato k di posizioni. 
+# La rotazione verso sinistra significa che ciascun elemento della lista viene spostato a sinistra di una posizione
+# e l'elemento iniziale viene spostato alla fine della lista. Per la rotazione utilizzare lo slicing e gestire il caso
+# in cui il numero specificato di posizioni sia maggiore della lunghezza della lista.
+
+def rotate_left(elements: list, k: int) -> list:
+    n = len(elements)
+    
+    k = k % n
+    
+    rotated_elements = elements[k:] + elements[:k]
+    return rotated_elements
+
+
+print(rotate_left([1, 2, 3, 4, 5], 2))
+
+
 	
 
 
