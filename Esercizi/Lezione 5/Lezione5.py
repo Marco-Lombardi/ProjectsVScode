0d54e01061a2ebbe139edc37c50502ee0d0186ce
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-06 12:15:13 +0200

    Exercise 8

--- Esercizi/Lezione 5/Lezione5.py
+++ Esercizi/Lezione 5/Lezione5.py
@@ -109,6 +109,31 @@ def check_parentheses(s):
 print(check_parentheses("()()"))
 print(check_parentheses("(()))("))
 
+# Exercise 8
+#Scrivi una funzione che conta e ritorna quante volte un elemento appare isolato in una lista di numeri interi. 
+# Un elemento è considerato isolato se non è affiancato sia a destra che a sinistra da elementi uguali.
+
+def count_isolated(numbers): 
+    count = 0
+    
+    
+    for i in range(len(numbers)):
+        
+        if i == 0 and numbers[i] != numbers[i+1]:  
+            count += 1
+        elif i == len(numbers) - 1 and numbers[i] != numbers[i-1]:  
+            count += 1
+        elif numbers[i] != numbers[i-1] and numbers[i] != numbers[i+1]:  
+            count += 1
+    
+    return count
+
+print(count_isolated([1, 2, 2, 3, 3, 3, 4]))
+
+print(count_isolated([1, 2, 3, 4, 5]))
+
+
+
 
     
     
