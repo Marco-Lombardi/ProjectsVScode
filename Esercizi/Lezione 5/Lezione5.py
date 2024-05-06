16504bd2575f9d70875379d30c70745125272e1a
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-06 12:00:51 +0200

    Exercise 6

--- Esercizi/Lezione 5/Lezione5.py
+++ Esercizi/Lezione 5/Lezione5.py
@@ -78,6 +78,17 @@ print(check_access("admin", "54321", True))
 # Scrivi una funzione che, dato un numero intero, determina se è un "numero magico". Un numero magico è 
 # definito come un numero che contiene il numero 7.
 
+def is_magic_number(num: int) -> bool:
+    number_str = str(num)
+    if '7' in number_str:
+        return True
+    else:
+        return False
+    
+print(is_magic_number(70))
+
+print(is_magic_number(123))
+    
     
 
 
