839c83a8e2f552222662d730c6b4d0f5627a4e26
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-06 11:57:59 +0200

    Exercise 5

--- Esercizi/Lezione 5/Lezione5.py
+++ Esercizi/Lezione 5/Lezione5.py
@@ -57,6 +57,24 @@ numbers: list[int] = [1, 2, 3, 4, 5]
 for i in range(len(numbers)):
     print('Number:', numbers[i])
 
+# Exercise 5
+
+# Scrivi una funzione che accetti tre parametri: username, password e status di attivazione dell'account 
+# (attivo/non attivo). L'accesso è consentito solo se il nome utente è "admin", la password corrisponde a "12345" e 
+# l'account è attivo. La funzione ritorna "Accesso consentito" oppure "Accesso negato
+
+def check_access(username: str, password: ..., is_active: bool) -> str:
+    if username == "admin" and password == "12345" and is_active == True:
+        return "Accesso consentito"
+    else:
+        return "Accesso negato"
+
+print(check_access("admin", "12345", True))
+
+print(check_access("admin", "54321", True))
+
+    
+
 
 
     
