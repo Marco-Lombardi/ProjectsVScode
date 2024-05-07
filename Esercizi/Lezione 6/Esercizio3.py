accaa22e9a7122c710ef99f3e01b11821bbc52ae
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-07 13:53:27 +0200

    Esercizio 3

---
+++ Esercizi/Lezione 6/Esercizio3.py
@@ -0,0 +1,16 @@
+# Marco Lombardi
+# 07/05/2024
+class Animal:
+
+  def __init__(self, name: str, family: str, animals: str):
+    self.name = name
+    self.family = family
+    self.animals = animals
+
+    
+
+
+  
+  
+  
+  
