6b8523cb6f362bff54c3bf99a8382cf8db8e10ff
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-08 13:36:36 +0200

    Creazione classi zoo

---
+++ Esercizi/zoo.py
@@ -0,0 +1,21 @@
+class Zoo:
+
+    def __init__(fences, zoo_keeper,):
+
+class Animal:
+
+    def __init__(name, speciec, age, widht, preferred_habitat, healt):
+
+class Fence:
+
+    def __init__(area, temperature, habitat):
+
+class Zookeeper:
+
+    def __init__(name, surname, id):
+        
+
+
+
+   
+                
