33787e91146aaede61ba80171577583caea4e5ac
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-13 19:02:02 +0200

    Esercizio ITS

---
+++ ITS/building.py
@@ -0,0 +1,17 @@
+from room import Room
+
+class Building:
+    def __init__(self, none:, adress: str, floors: int):
+    self.name: str = name  
+    self.adress: address = address
+    self.floors: int = floors
+    self.rooms: list[Room] = []
+
+def get_name(self) -> str:
+    return self.name
+
+def get_adress(self) -> str:
+   return self.adress
+
+def get_floors(self) -> str:
+    return self.floors 
---
+++ ITS/room.py
@@ -0,0 +1,28 @@
+def __init__(self, none: str, floor: int, num_sets: int):
+    self.set_nome(nome)
+    self.set_floor(floor)
+    self.set_num_seats(num_sets)
+
+def set__nome(self, none: str):
+    self.nome = none
+
+def set_floor(self, floor: int):
+    self.floor = floor
+
+def set_num_seats(self, num_setas: int)
+    self_num_seats = set_num_seats
+
+def get_nome(self) -> str:
+    return self.name
+
+def get_floor(self) -> int:
+    return self.floor
+
+def get_num_seats(self) -> int:
+    return self.num_seats   
+
+def get_rooms(self) -> list[Room]:
+    return self.rooms
+
+def __str__(self) -> str:
+    s: str = f'Bulding(name=(self.get_names()))'                
