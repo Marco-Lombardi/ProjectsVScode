f03f79e2f581153693c960d1d94584ff50d92ef0
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-15 18:00:49 +0200

    Progetto ITS

--- ITS/room.py
+++ ITS/room.py
@@ -1,28 +1,27 @@
-def __init__(self, none: str, floor: int, num_sets: int):
-    self.set_nome(nome)
-    self.set_floor(floor)
-    self.set_num_seats(num_sets)
-
-def set__nome(self, none: str):
-    self.nome = none
-
-def set_floor(self, floor: int):
-    self.floor = floor
-
-def set_num_seats(self, num_setas: int)
-    self_num_seats = set_num_seats
-
-def get_nome(self) -> str:
-    return self.name
-
-def get_floor(self) -> int:
-    return self.floor
-
-def get_num_seats(self) -> int:
-    return self.num_seats   
-
-def get_rooms(self) -> list[Room]:
-    return self.rooms
-
-def __str__(self) -> str:
-    s: str = f'Bulding(name=(self.get_names()))'                
+class Room:
+    
+    def __init__(self, name: str, floor: int, num_seats: int):
+        self.set_name(name)
+        self.set_floor(floor)
+        self.set_num_seats(num_seats)
+        
+    def set_name(self, name: str):
+        self.name = name
+    
+    def set_floor(self, floor: int):
+        self.floor = floor
+        
+    def set_num_seats(self, num_seats: int):
+        self.num_seats = num_seats
+        
+    def get_name(self) -> str:
+        return self.name
+    
+    def get_floor(self) -> int:
+        return self.floor
+    
+    def get_num_seats(self) -> int:
+        return self.num_seats
+    
+    def __str__(self) -> str:
+        return f'Room(name={self.get_name()}, floor={self.get_floor()}, num_seats={self.get_num_seats()})'
