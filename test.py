99ccc323ac4e68ba0fb9af16df3c645eebc2a45f
Marco-Lombardi <marcolombardi25@libero.it>
2024-05-31 16:08:40 +0200

    Aggiornamento test

--- test.py
+++ test.py
@@ -11,6 +11,19 @@ class TestCalc(unittest.TestCase):
         
         self.assertEqual(result, 5, msg=f"Error Test Failde the goal was 5, the function returned {result}")    
     
+    def test_diff(self):
+        
+        calc_1 = Calculations(a=2, b=3)
+        result = calc_1.get_difference()
+        
+        self.assertEqual(result, 5, msg=f"Error Test Failde the goal was 5, the function returned {result}")    
+    
+    def test_div(self):
+        
+        calc_1 = Calculations(a=2, b=3)
+        result = calc_1.get_product()
+        
+        self.assertEqual(result, 5, msg=f"Error Test Failde the goal was 5, the function returned {result}")    
     
 if __name__ == "__main__":
     
